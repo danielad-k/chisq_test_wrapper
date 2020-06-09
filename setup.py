f6a042cdc81a38f0002d5e5fee05e5bf737a791a
danielad-k <53984115+danielad-k@users.noreply.github.com>
2020-06-09 00:05:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='chisq_test_wrapper',
     version='',
-    packages=['statsmodels', 'scipy'],
+    packages=[],
     url='https://github.com/neuhofmo/chisq_test_wrapper',
     license='',
     author='',
     author_email='',
     description='A friendly, automated chi-square test function which takes care of post-hoc tests and multiple comparisons',
-    requires=['statsmodels', 'scipy']
+    requires=[]
 )
